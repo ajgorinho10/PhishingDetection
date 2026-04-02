bda1996c9d61124daa12a27d5f82f6c52234f8a4
ajgorinho10 <kowalskimarek0909@gmail.com>
2026-04-02 17:45:38 +0200

    .

--- src/CNN.py
+++ src/CNN.py
@@ -1,159 +1,199 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
-import numpy as np
 import time
 
-from Set_Processor import ImportData
+import torch
+from sklearn.metrics import classification_report, confusion_matrix
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset, DataLoader
+
+from Set_Processor import ImportSet1, ImportData
 
 
 class CNN(nn.Module):
-    def __init__(self, vocab_size, embed_dim=64, max_len=100):
+    def __init__(self, embed_dim=16, max_len=100, epoch = 5):
         super(CNN, self).__init__()
-        # Warstwa osadzeń - zamienia numery liter na wielowymiarowe wektory
-        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_dim)
 
-        # Skaner konwolucyjny (Skanuje zbitki po 3 litery naraz)
-        self.conv1 = nn.Conv1d(in_channels=embed_dim, out_channels=128, kernel_size=3, padding=1)
-        self.relu = nn.ReLU()
-        # Zmniejszanie wymiarowości (wyciąganie najważniejszych cech)
+        self.vocab_size = None
+        self.embed_dim = embed_dim
+        self.max_len = max_len
+        self.slownik = None
+        self.epoch = epoch
+
+        self.embed = None
+
+        self.conv1 = nn.Conv1d(in_channels=embed_dim, out_channels=embed_dim * 2, kernel_size=2, padding='same')
+        self.conv2 = nn.Conv1d(in_channels=embed_dim * 2, out_channels=embed_dim * 4, kernel_size=3, padding='same')
+        self.conv3 = nn.Conv1d(in_channels=embed_dim * 4, out_channels=embed_dim * 8, kernel_size=5, padding='same')
+        self.conv4 = nn.Conv1d(in_channels=embed_dim * 8, out_channels=embed_dim * 16, kernel_size=7, padding='same')
+
         self.pool = nn.MaxPool1d(kernel_size=2)
+        self.dropout = nn.Dropout(p=0.3)
+        self.relu = nn.ReLU()
+        self.gelu = nn.GELU()
 
-        # Ochrona przed przeuczeniem
-        self.dropout = nn.Dropout(0.3)
+        linearInput = embed_dim * 16 * (self.max_len//4)
+        self.linear1 = nn.Linear(in_features=linearInput, out_features=linearInput//8)
+        self.linear2 = nn.Linear(in_features=linearInput//8, out_features=1)
 
-        # Klasyfikator końcowy (MLP na końcu sieci)
-        self.fc = nn.Linear(128 * (max_len // 2), 1)
 
-    def forward(self, x):
-        x = self.embedding(x)  # Kształt: [batch, max_len, embed_dim]
-        x = x.permute(0, 2, 1)  # Wymagane przez Conv1d: [batch, kanały, max_len]
+        self.sigmoid = nn.Sigmoid()
 
+    def forward(self,x):
+        x = self.embed(x)
+        x = x.permute(0,2,1)
+
+        #KONWULENCYJNA - 1
         x = self.conv1(x)
-        x = self.relu(x)
-        x = self.pool(x)
+        #x = self.relu(x)
+        x = self.gelu(x)
 
-        x = torch.flatten(x, 1)  # Spłaszczanie danych przed warstwą liniową
+        #KONWULENCYJNA - 2 po niej dropout i pool
+        x = self.conv2(x)
+        #x = self.relu(x)
+        x = self.gelu(x)
+
+        x = self.pool(x)
         x = self.dropout(x)
-        x = self.fc(x)
+
+        #KONWULENCYJNA - 3
+        x = self.conv3(x)
+        #x = self.relu(x)
+        x = self.gelu(x)
+
+        #KONWULENCYJNA - 4 po niej dropout i pool
+        x = self.conv4(x)
+        #x = self.relu(x)
+        x = self.gelu(x)
+
+        x = self.pool(x)
+        x = self.dropout(x)
+
+        #MLP - dwie warstwy po pierwszej dropout
+        x = torch.flatten(x, 1)
+        x = self.linear1(x)
+        #x = self.relu(x)
+        x = self.gelu(x)
+        x = self.dropout(x)
+
+        x = self.linear2(x)
+
+
+
+        x = self.sigmoid(x)
         return x
 
 
-# ==========================================
-# 2. FUNKCJA TRENUJĄCA I PRZYGOTOWUJĄCA DANE
-# ==========================================
-def train_CNN_sequence(X_data, y_data):
-    print("\n" + "=" * 60)
-    print("🚀 TRENOWANIE SIECI KONWOLUCYJNEJ (CNN 1D) NA SEKWENCJACH ZNAKÓW")
-    print("=" * 60)
+    def tokenize(self, data):
+        surowe_znaki = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+        unikalne_znaki = list(set(surowe_znaki))
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"🔥 PyTorch używa akceleratora: {device}")
+        char_to_int = {c: i+1 for i, c in enumerate(sorted(unikalne_znaki))} if self.slownik is None else self.slownik
+        self.slownik = char_to_int
 
-    # --- KROK 1: Tokenizacja Znakowa i Padding ---
-    print("\nBudowanie słownika znaków i tokenizacja linków...")
+        self.vocab_size = len(unikalne_znaki) + 1
+        self.embed = nn.Embedding(self.vocab_size, self.embed_dim)
 
-    # Kuloodporne tworzenie słownika (set() usuwa ewentualne duplikaty znaków)
-    surowe_znaki = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
-    unikalne_znaki = list(set(surowe_znaki))  # Pozbywamy się duplikatów
+        X_sequences = []
+        for url in data:
+            seq = [char_to_int.get(c.lower(),0) for c in url]
 
-    char_to_int = {c: i + 1 for i, c in enumerate(unikalne_znaki)}
-    vocab_size = len(char_to_int) + 1
-    max_len = 300
+            seq = seq[:self.max_len]
 
-    X_sequences = []
-    for url in X_data:
-        seq = [char_to_int.get(c.lower(), 0) for c in str(url)]
-        seq = seq[:max_len]
-        if len(seq) < max_len:
-            seq += [0] * (max_len - len(seq))
-        X_sequences.append(seq)
+            if len(seq) < self.max_len:
+                seq += [0]* (self.max_len - len(seq))
+            X_sequences.append(seq)
 
-    X_seq_array = np.array(X_sequences)
+        return X_sequences
 
-    # --- KROK 2: Podział danych ---
-    X_train, X_test, y_train, y_test = train_test_split(X_seq_array, y_data, test_size=0.3, random_state=42)
+    def train_model(self, X_data, y_data):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("[Device]:", device)
 
-    # --- KROK 3: Zamiana na Tensory PyTorch ---
-    print("Wysyłanie danych na kartę graficzną...")
-    X_train_tensor = torch.tensor(X_train, dtype=torch.long)  # Zauważ dtype=torch.long! Wymagane dla Embeddingów
-    y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32).view(-1, 1)
+        print("[Przygotowywanie danych]: Tokenizacja, podział danych")
+        X_tokenized = self.tokenize(X_data)
 
-    X_test_tensor = torch.tensor(X_test, dtype=torch.long).to(device)
-    y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).view(-1, 1).to(device)
+        X_train, X_test, y_train, y_test = train_test_split(X_tokenized, y_data, test_size=0.3, random_state=42)
 
-    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
-    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+        print("[Przygotowywanie danych]: Zamiana danych na tensory PyTorch")
 
-    # --- KROK 4: Inicjalizacja modelu ---
-    model = CNN(vocab_size=vocab_size, max_len=max_len).to(device)
-    criterion = nn.BCEWithLogitsLoss()
-    #optimizer = optim.Adam(model.parameters(), lr=0.002)
-    optimizer = optim.SGD(model.parameters(),lr=0.01,momentum=0.9)
+        X_train_tensor = torch.tensor(X_train,dtype=torch.long)
+        X_test_tensor = torch.tensor(X_test,dtype=torch.long).to(device)
 
-    # --- KROK 5: Pętla ucząca ---
-    epochs = 10
-    print("\nRozpoczynamy trening sieci CNN...")
-    start_time = time.time()
+        y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32).view(-1,1)
+        y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).view(-1,1).to(device)
 
-    for epoch in range(epochs):
-        model.train()
-        running_loss = 0.0
+        train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+        train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 
-        for batch_X, batch_y in train_loader:
-            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+        print("[Przygotowywanie danych]: Zakończone !")
 
-            optimizer.zero_grad()
-            outputs = model(batch_X)
-            loss = criterion(outputs, batch_y)
-            loss.backward()
-            optimizer.step()
+        print("[Trening modelu]: Inicjalizacja modelu")
+        self.to(device)
+        #optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=0.001)
+        criterion = nn.BCELoss()
 
-            running_loss += loss.item()
+        start_time = time.time()
+        print("[Trening modelu]: Rozpoczynanie treningu")
+        for epoch in range(self.epoch):
+            self.train()
+            running_loss = 0.0
 
-        print(f"Epoka [{epoch + 1}/{epochs}] | Błąd (Loss): {running_loss / len(train_loader):.4f}")
+            for batch_X, batch_y in train_loader:
+                batch_X, batch_y = batch_X.to(device), batch_y.to(device)
 
-    czas = time.time() - start_time
-    print(f"✅ Trening zakończony w {czas:.2f} sekund!")
+                optimizer.zero_grad()
+                output = self(batch_X)
+                loss = criterion(output, batch_y)
+                loss.backward()
+                optimizer.step()
 
-    # --- KROK 6: Ewaluacja ---
-    print("\nTestowanie modelu CNN w paczkach (ochrona VRAM)...")
-    model.eval()
+                running_loss += loss.item()
 
-    # 1. Tworzymy DataLoader również dla zbioru testowego!
-    # Używamy większego batch_size (np. 512), bo w fazie testów nie liczymy gradientów, więc zużywa mniej pamięci
-    test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
-    test_loader = DataLoader(test_dataset, batch_size=512, shuffle=False)
+            print(f"Epoch {epoch+1}/{self.epoch} || loss: {running_loss/len(train_loader):.4f}")
 
-    wszystkie_predykcje = []
-    wszystkie_etykiety = []
+        czas = time.time() - start_time
+        print(f"[Trening modelu]: Trening zakończony w {czas:.2f}s")
 
-    with torch.no_grad():
-        for batch_X, batch_y in test_loader:
-            # Wysyłamy TYLKO paczkę 512 linków na kartę graficzną
-            batch_X = batch_X.to(device)
+        print(f"[Wyniki]: ...")
+        self.eval()
+        test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+        test_loader = DataLoader(test_dataset, batch_size=512, shuffle=False)
 
-            # Predykcja dla małej paczki
-            predictions_raw = model(batch_X)
-            predictions = torch.sigmoid(predictions_raw).round().cpu().numpy()
+        wszystkie_predykcje = []
+        wszystkie_etykiety = []
 
-            # Zbieramy wyniki do wspólnej listy
-            wszystkie_predykcje.extend(predictions)
-            wszystkie_etykiety.extend(batch_y.cpu().numpy())
+        with torch.no_grad():
+            for batch_X, batch_y in test_loader:
+                batch_X = batch_X.to(device)
 
-    print("\n" + "=" * 50)
-    print("RAPORT: PyTorch CNN 1D (Znakowa tokenizacja)")
-    print("=" * 50)
-    print(classification_report(wszystkie_etykiety, wszystkie_predykcje))
-    print("Macierz Pomyłek:")
-    print(confusion_matrix(wszystkie_etykiety, wszystkie_predykcje))
+                pred_raw = self(batch_X)
+                pred = pred_raw.round().cpu().numpy()
+
+                wszystkie_predykcje.extend(pred)
+                wszystkie_etykiety.extend(batch_y.cpu().numpy())
+
+        print("\n" + "=" * 50)
+        print("RAPORT: PyTorch CNN 1D (Znakowa tokenizacja)")
+        print("=" * 50)
+        print(classification_report(wszystkie_etykiety, wszystkie_predykcje))
+        print("Macierz Pomyłek:")
+        print(confusion_matrix(wszystkie_etykiety, wszystkie_predykcje))
+
+
 
 
 if __name__ == "__main__":
-    d = ImportData()
-    d.Import_set_3()
-    X, y = d.Get_NLP()
-    train_CNN_sequence(X, y)
+    data = ImportData()
+    data.Import_set_3()
+    X, y = data.Get_NLP()
+
+    CNN = CNN(embed_dim=64, max_len=100,epoch=10)
+    CNN.train_model(X,y)
+
+
---
+++ src/LSTMTest.py
@@ -0,0 +1,12 @@
+
+
+from Set_Processor import ImportData
+
+import torch.nn as nn
+
+
+class LSTM(nn):
+    def __init__(self):
+        super(LSTM,self).__init__()
+        
+        
